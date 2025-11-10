b146025f6d88bbb99f451ff1890d198ec94f7d8b
1F616EMO <root@1f616emo.xyz>
2025-11-10 19:29:46 +0800

    Remove unused functions

--- init.lua
+++ init.lua
@@ -3,12 +3,9 @@
 -- Copyright (c) 2022, 2024-2025  1F616EMO
 -- SPDX-LICENSE-IDENTIFIER: LGPL-2.1-OR-LATER
 
-local S = core.get_translator("basket")
-local F = core.formspec_escape
-local FS = function(...) return F(S(...)) end
-local MP = core.get_modpath("basket")
-
 basket = {}
+
+local MP = core.get_modpath("basket")
 
 dofile(MP .. DIR_DELIM .. "src" .. DIR_DELIM .. "api.lua")
 dofile(MP .. DIR_DELIM .. "src" .. DIR_DELIM .. "node.lua")
