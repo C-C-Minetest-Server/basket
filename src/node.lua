3ce07bb6f658d33fad7b7d20eef5dd55e7ec5fea
1F616EMO <root@1f616emo.xyz>
2026-02-05 13:42:57 +0800

    Fix wrong reference to MTG default
    Fixes #4

--- src/node.lua
+++ src/node.lua
@@ -129,7 +129,7 @@ local node_def = {
     stack_max = 1,
     on_blast = function() end,
     on_drop = function(itemstack) return itemstack end,
-    sounds = default.node_sound_wood_defaults(),
+    sounds = xcompat.node_sound_wood_defaults(),
 }
 
 if core.global_exists("pipeworks") then
